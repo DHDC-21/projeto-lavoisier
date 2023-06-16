021238341f2a5ce05a36f9569e4c316d80458858
annale <annale@email.com>
2023-06-16 18:09:42 -0300

    2023-06-16: CRUD funcionando!

--- src/routes/client.routes.js
+++ src/routes/client.routes.js
@@ -44,12 +44,12 @@ router
 		const clientId = req.params.id;
 	
 		// Atualização do cliente usando o método update()
-		const updatedClient = await Client.update(
+		const deleteClient = await Client.update(
 			{ id, nome, endereco, telefone, CPF },
 			{ where: { id: clientId } }
 		);
   
-	  	console.log('Cliente atualizado:', updatedClient);
+	  	console.log('Cliente atualizado:', deleteClient);
 	  	res.redirect('/clients');
 	} catch (error) {
 	  	console.error('Erro ao atualizar cliente:', error);
@@ -62,5 +62,21 @@ router
 	res.render("clientes_delete",{title:"Deletar Clientes",clientId});
 })
 
+.post("/delete/:id", async (req,res)=>{
+	try {
+		const clientId = req.params.id;
+	
+		// Atualização do cliente usando o método update()
+		const deleteClient = await Client.destroy(
+			{ where: { id: clientId } }
+		);
+  
+	  	console.log('Cliente deletado:', deleteClient);
+	  	res.redirect('/clients');
+	} catch (error) {
+	  	console.error('Erro ao deletar cliente:', error);
+	  	res.status(500).send('Erro ao deletar cliente.');
+	}
+})
 
 module.exports = router;
