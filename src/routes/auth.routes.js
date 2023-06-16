4728cea63b402d07b6b0f1de3fe10cabaf1ff301
annale <annale@email.com>
2023-06-16 18:37:54 -0300

    2023-06-16: login quase lá

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,32 +2,36 @@
 const { Router } = require("express");
 const router = Router();
 
+const bcrypt = require('bcrypt');
+const User = require("../models/Client");
+
 
 router
 .get("/",(req,res)=>{
 	res.render("login",{title:"Authenticação"})
 })
-.post("/login", (req, res) => {
-	const { username, password } = req.body; // Supondo que você esteja enviando o nome de usuário e a senha no corpo da solicitação
+.post("/login", async (req, res) => {
+	const { username, password } = req.body;
 
-	// Aqui você pode adicionar a lógica para validar o usuário, verificar o nome de usuário e a senha em um banco de dados, por exemplo
-
-	if (username == User.findAll({
-		atributes: "usuario",
-		where:{
-			usuario: username}
-	}) && password == User.findAll({
-		atributes: "senha",
-		where:{
-			senha: password}})) {
-
-		// Usuário válido
-		//res.status(200).send("Usuário válido");
+	try {
+	  // Buscar usuário pelo nome de usuário
+	  const user = await User.findOne({
+		where: {
+			usuario: username }
+	});
+  
+	  // Verificar se o usuário existe e se a senha corresponde
+	  if (user && await bcrypt.compare(password, user.senha)) {
+		// Autenticação bem-sucedida
+		res.json({ message: 'Autenticação bem-sucedida' });
 		res.redirect("/clients");
-	} else {
-		// Usuário  invalido
-		res.status(401).send("Usuário inválido");
-		res.render("login");
+	  } else {
+		// Falha na autenticação
+		res.status(401).json({ message: 'Falha na autenticação' });
+	  }
+	} catch (error) {
+	  console.error('Erro de autenticação:', error);
+	  res.status(500).json({ message: 'Erro de autenticação' });
 	}
 })
 
